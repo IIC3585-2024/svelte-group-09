1efe2993bf2d8910e67fe202b0438e76a0e93f26
sbvillalobosm <sbvillalobosm@uc.cl>
2024-06-07 11:19:30 -0400

    feat: Create new view for activities

--- src/routes/Activities.svelte
+++ src/routes/Activities.svelte
@@ -2,6 +2,8 @@
   import { onMount } from 'svelte';
   import FaHeart from 'svelte-icons/fa/FaHeart.svelte';
   import FaRegHeart from 'svelte-icons/fa/FaRegHeart.svelte';
+  import { goto } from '$app/navigation';
+
   export let data;
 
   let favorites = [];
@@ -22,6 +24,10 @@
     }
     localStorage.setItem('favorites', JSON.stringify(favorites));
   }
+
+  function openActivityDetails(activity) {
+    goto(`/activity/${activity.id}`);
+  }
 </script>
 
 <div class="ml-4 mt-4">
@@ -29,18 +35,18 @@
   {#if data && data.results}
     <div class="flex flex-col gap-4 overflow-y-auto h-[350px]">
       {#each data.results as activity}
-        <div class="flex border border-gray-400 shadow p-2 h-24">
+        <div class="flex border border-gray-400 shadow p-2 h-24 cursor-pointer" on:click={() => openActivityDetails(activity)}>
           <div class="flex flex-col text-pink-400 w-4/5">
             <p class="uppercase font-semibold">{activity.title}</p>
             <p>Santiago de Chile</p>
           </div>
-          <div class="w-1/5 self-center">
+          <div class="w-1/5 self-center" on:click|stopPropagation={() => toggleFavorite(activity)}>
             {#if favorites.includes(activity.id)}
-              <div class="text-red-500 w-6 h-6 mx-auto cursor-pointer" on:click={() => toggleFavorite(activity)}>
+              <div class="text-red-500 w-6 h-6 mx-auto cursor-pointer">
                 <FaHeart />
               </div>
             {:else}
-              <div class="text-red-500 w-6 h-6 mx-auto cursor-pointer" on:click={() => toggleFavorite(activity)}>
+              <div class="text-red-500 w-6 h-6 mx-auto cursor-pointer">
                 <FaRegHeart />
               </div>
             {/if}
