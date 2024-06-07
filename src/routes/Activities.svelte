46492ca627474517fe0cae9b3072fc7ef2a3301a
Josecaceresb <jose.caceresb@uc.cl>
2024-06-07 00:41:37 -0400

    chore: Update npm dependencies and SvelteKit configuration

--- src/routes/Activities.svelte
+++ src/routes/Activities.svelte
@@ -1,5 +1,27 @@
 <script>
+  import { onMount } from 'svelte';
+  import FaHeart from 'svelte-icons/fa/FaHeart.svelte';
+  import FaRegHeart from 'svelte-icons/fa/FaRegHeart.svelte';
   export let data;
+
+  let favorites = [];
+
+  onMount(() => {
+    const storedFavorites = localStorage.getItem('favorites');
+    if (storedFavorites) {
+      favorites = JSON.parse(storedFavorites);
+    }
+  });
+
+  function toggleFavorite(activity) {
+    const index = favorites.indexOf(activity.id);
+    if (index === -1) {
+      favorites = [...favorites, activity.id];
+    } else {
+      favorites = favorites.filter(fav => fav !== activity.id);
+    }
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }
 </script>
 
 <div class="ml-4 mt-4">
@@ -7,10 +29,21 @@
   {#if data && data.results}
     <div class="flex flex-col gap-4 overflow-y-auto h-[350px]">
       {#each data.results as activity}
-        <div class="border border-gray-400 shadow p-2 h-24">
-          <div class="flex flex-col text-pink-400">
+        <div class="flex border border-gray-400 shadow p-2 h-24">
+          <div class="flex flex-col text-pink-400 w-4/5">
             <p class="uppercase font-semibold">{activity.title}</p>
             <p>Santiago de Chile</p>
+          </div>
+          <div class="w-1/5 self-center">
+            {#if favorites.includes(activity.id)}
+              <div class="text-red-500 w-6 h-6 mx-auto cursor-pointer" on:click={() => toggleFavorite(activity)}>
+                <FaHeart />
+              </div>
+            {:else}
+              <div class="text-red-500 w-6 h-6 mx-auto cursor-pointer" on:click={() => toggleFavorite(activity)}>
+                <FaRegHeart />
+              </div>
+            {/if}
           </div>
         </div>
       {/each}
